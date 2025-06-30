5ebf675028d4c4d7402e339bc5bf7338312e9c76
Felix Salim <felixsalim64@gmail.com>
2025-06-30 14:03:32 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.template')
+@section('title', 'Dashboard')
+@section('content')
+    <h1>Welcome to the Dashboard</h1>
+    <p>This is your dashboard where you can manage your tasks and projects.</p>
+@endsection
---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+    
+    {{-- Local CSS --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    @yield('extra-css')
+</head>
+<body class="min-vh-100 d-flex flex-column m-0 p-0">
+    <div class="row m-0 p-0 min-vh-100 w-100">
+        <div class="d-flex flex-column flex-shrink-0 p-3 bg-light m-0 sidebar-app">
+            @include('layout.navbar')
+        </div>
+        <div class="col">
+            <main class="flex-fill">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    @yield('extra-script')
+</body>
+</html>
