278f3b0108db1be8c24f3209ae20d9bdb36762f5
Felix Salim <felixsalim64@gmail.com>
2025-08-02 14:31:42 +0700

    fix: storage

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -74,15 +74,10 @@ class AuthController extends Controller
     public function updateProfilePicture(Request $request) {
         $user = auth()->user();
 
-        // Ensure the storage link is created
-        if (!file_exists(public_path('storage'))) {
-            Artisan::call('storage:link');
-        }
-
 
         if($request->hasFile('profile_picture')) {
             $file = $request->file('profile_picture');
-            $path = $file->store('profile_picture', 'public');
+            $path = $file->store('profile_picture', ['disk' => 'profile_picture']);
 
             $user->profile_picture = $path;
             $user->save();
